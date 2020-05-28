e9ab5bea9c575ff899d91bbdfbe7b68bef6610ec
nate contino <nathan.contino@10gen.com>
2020-05-28 09:24:46 -0600

    (DOCSP-9099): Kotlin android tutorial code (#8)

---
+++ kotlin-android/app/src/main/java/com/mongodb/tasktracker/TaskTracker.kt
@@ -0,0 +1,35 @@
+package com.mongodb.tasktracker
+
+import android.app.Application
+
+import io.realm.Realm
+import io.realm.RealmApp
+import io.realm.RealmAppConfiguration
+import io.realm.examples.objectserver.BuildConfig
+import io.realm.log.LogLevel
+import io.realm.log.RealmLog
+
+lateinit var taskApp: RealmApp
+
+inline fun <reified T> T.TAG(): String = T::class.java.simpleName
+
+/*
+ * TaskTracker: Sets up the taskApp Realm App and enables Realm-specific logging in debug mode.
+ */
+class TaskTracker : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        taskApp = RealmApp(RealmAppConfiguration.Builder(BuildConfig.MONGODB_REALM_APP_ID)
+                .baseUrl(BuildConfig.MONGODB_REALM_URL)
+                .appName(BuildConfig.VERSION_NAME)
+                .appVersion(BuildConfig.VERSION_CODE.toString())
+                .build())
+
+        // Enable more logging in debug mode
+        if (BuildConfig.DEBUG) {
+            RealmLog.setLevel(LogLevel.ALL)
+        }
+    }
+}
