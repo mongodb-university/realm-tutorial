cd878dbd42029cd8bd088894c8cd51c207a299a2
nate contino <nathan.contino@mongodb.com>
2020-06-02 13:54:48 -0600

    Updating to match tutorial text exactly (#19)

--- kotlin-android/app/src/main/java/com/mongodb/tasktracker/TaskTracker.kt
+++ kotlin-android/app/src/main/java/com/mongodb/tasktracker/TaskTracker.kt
@@ -1,35 +1,39 @@
 package com.mongodb.tasktracker
 
 import android.app.Application
+import android.util.Log
 
 import io.realm.Realm
 import io.realm.RealmApp
 import io.realm.RealmAppConfiguration
-import io.realm.examples.objectserver.BuildConfig
 import io.realm.log.LogLevel
 import io.realm.log.RealmLog
 
 lateinit var taskApp: RealmApp
 
+// global Kotlin extension that resolves to the short version
+// of the name of the current class. Used for labelling logs.
 inline fun <reified T> T.TAG(): String = T::class.java.simpleName
 
 /*
- * TaskTracker: Sets up the taskApp Realm App and enables Realm-specific logging in debug mode.
- */
+* TaskTracker: Sets up the taskApp Realm App and enables Realm-specific logging in debug mode.
+*/
 class TaskTracker : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
         taskApp = RealmApp(RealmAppConfiguration.Builder(BuildConfig.MONGODB_REALM_APP_ID)
-                .baseUrl(BuildConfig.MONGODB_REALM_URL)
-                .appName(BuildConfig.VERSION_NAME)
-                .appVersion(BuildConfig.VERSION_CODE.toString())
-                .build())
+            .baseUrl(BuildConfig.MONGODB_REALM_URL)
+            .appName(BuildConfig.VERSION_NAME)
+            .appVersion(BuildConfig.VERSION_CODE.toString())
+            .build())
 
         // Enable more logging in debug mode
         if (BuildConfig.DEBUG) {
             RealmLog.setLevel(LogLevel.ALL)
         }
+
+        Log.v(TAG(), "Initialized the Realm App configuration for: ${taskApp.configuration.appId}")
     }
 }
--- kotlin-android/app/src/main/java/com/mongodb/tasktracker/model/Task.kt
+++ kotlin-android/app/src/main/java/com/mongodb/tasktracker/model/Task.kt
@@ -5,16 +5,12 @@ import io.realm.annotations.PrimaryKey
 import io.realm.annotations.Required
 import org.bson.types.ObjectId
 
-enum class TaskStatus(val displayName: String) {
-    Open("Open"),
-    InProgress("In Progress"),
-    Complete("Complete"),
-}
 
 open class Task(_name: String = "Task", project: String = "My Project") : RealmObject() {
     @PrimaryKey var _id: ObjectId = ObjectId()
     var _partition: String = project
     var name: String = _name
+
     @Required
     private var status: String = TaskStatus.Open.name
     var statusEnum: TaskStatus
