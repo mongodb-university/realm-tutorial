f42b12e0e5dad81fd51f5aeb967f91206a160db2
Nick Larew <nick.larew@mongodb.com>
2020-07-28 17:33:31 -0400

    Web: Apollo Client 2 -> 3

--- web/src/realm/RealmApolloProvider.tsx
+++ web/src/realm/RealmApolloProvider.tsx
@@ -3,9 +3,13 @@ import * as Realm from "realm-web";
 import { useRealmApp } from "./RealmApp";
 
 // Apollo
-import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from "apollo-boost";
-import { setContext } from "apollo-link-context";
-import { ApolloProvider } from "@apollo/react-hooks";
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { NormalizedCacheObject } from "@apollo/client/cache";
 
 const RealmApolloProvider: React.FC = ({ children }) => {
   const { id, user } = useRealmApp();
@@ -21,22 +25,33 @@ const RealmApolloProvider: React.FC = ({ children }) => {
 export default RealmApolloProvider;
 
 // TODO: Implement createApolloClient()
-function createApolloClient(realmAppId: string, user: Realm.User): ApolloClient<NormalizedCacheObject> {
+function createApolloClient(
+  realmAppId: string,
+  user: Realm.User
+): ApolloClient<NormalizedCacheObject> {
   const graphql_url = `https://realm.mongodb.com/api/client/v2.0/app/${realmAppId}/graphql`;
-  const httpLink = new HttpLink({ uri: graphql_url });
   
-  const authorizationHeaderLink = setContext(async (_, { headers }) => {
-    await user.refreshCustomData();
-    return ({
-      headers: {
-        ...headers,
-        Authorization: `Bearer ${user.accessToken}`,
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: graphql_url,
+      fetch: async (uri: RequestInfo, options: RequestInit) => {
+        if (!options.headers) {
+          options.headers = {} as Record<string, string>;
+        }
+        // Refreshing custom data also ensures a valid access token
+        await user.refreshCustomData();
+        const authenticatedOptions: RequestInit = {
+          ...options,
+          headers: {
+            ...options.headers,
+            Authorization: `Bearer ${user.accessToken}`
+          }
+        }
+        return fetch(uri, authenticatedOptions);
       },
-    })
-  });
-  
-  return new ApolloClient({
-    link: authorizationHeaderLink.concat(httpLink),
+    }),
     cache: new InMemoryCache(),
   });
+
+  return client
 }
