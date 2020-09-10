55adf53edaef4effdb6b0207dd43f1c20cd39133
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-09-10 13:38:06 -0400

    Update rn/views/ProjectsView.js
    Co-authored-by: Chris Bush <chris.bush@10gen.com>

--- rn/views/ProjectsView.js
+++ rn/views/ProjectsView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { View } from "react-native";
 import { useAuth } from "../providers/AuthProvider";
 import { ListItem } from "react-native-elements";
