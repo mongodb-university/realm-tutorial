74b74633a6ae68235fc1405128cbccf316d02854
Nick Larew <nick.larew@mongodb.com>
2020-09-22 14:10:23 -0400

    (DOCSP-11948): Add explanatory comments for wrapped auth functions/state

--- web/src/RealmApp.js
+++ web/src/RealmApp.js
@@ -18,19 +18,25 @@ export const RealmAppProvider = ({ appId, children }) => {
   React.useEffect(() => {
     setApp(new Realm.App(appId));
   }, [appId]);
-  // Define count and refresh to hack around
+  
+  // Wrap the Realm.App object's user state with React state
   const [currentUser, setCurrentUser] = React.useState(app.currentUser);
-
   async function logIn(credentials) {
+    // Log in with the given credentials
     await app.logIn(credentials);
+    // If successful, app.currentUser is the user that just logged in
     setCurrentUser(app.currentUser);
   }
   async function logOut() {
+    // Log out the currently active user
     await app.currentUser?.logOut();
+    // If another user was logged in too, they're now the current user.
+    // Otherwise, app.currentUser is null.
     setCurrentUser(app.currentUser);
   }
 
   const wrapped = { ...app, currentUser, logIn, logOut };
+  
   return (
     <RealmAppContext.Provider value={wrapped}>
       {children}
